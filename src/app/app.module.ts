e140a08df35dd2cea095b19432c4467531d65891
Simon Couclet <simoncouclet@yahoo.com>
2021-02-28 19:15:35 +0100

    camera access

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { File } from '@ionic-native/file/ngx';
+import { Camera } from '@ionic-native/Camera/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { AppComponent } from './app.component';
@@ -16,7 +17,7 @@ import { environment } from '../environments/environment';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule],
-  providers: [SplashScreen, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [SplashScreen, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, File, Camera],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Camera, CameraOptions } from '@ionic-native/Camera/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.page.html',
@@ -10,9 +13,21 @@ export class ProfilPage implements OnInit {
 userId: string;
 users= [];
 email;
+
+  croppedImagePath = "";
+  isLoading = false;
+
+  imagePickerOptions = {
+    maximumImagesCount: 1,
+    quality: 50
+};
+
   constructor(
     public afDB: AngularFireDatabase,
     public afAuth: AngularFireAuth,
+    private camera: Camera,
+    public actionSheetController: ActionSheetController,
+    private file: File
   ) { 
     this.afAuth.authState.subscribe(auth => {
       if (!auth) {
@@ -39,4 +54,47 @@ email;
       });
     });
   }
+
+  pickImage(sourceType) {
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: sourceType,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      this.croppedImagePath = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  async selectImage() {
+    const actionSheet = await this.actionSheetController.create({
+      buttons: [{
+        text: 'Choisissez dans votre gallerie',
+        handler: () => {
+          this.pickImage(this.camera.PictureSourceType.PHOTOLIBRARY);
+        }
+      },
+      {
+        text: 'Prendre une photo',
+        handler: () => {
+          this.pickImage(this.camera.PictureSourceType.CAMERA);
+        }
+      },
+      {
+        text: 'Annuler',
+        role: 'cancel'
+      }
+      ]
+    });
+    await actionSheet.present();
+  }
+
+
+
+
 }
